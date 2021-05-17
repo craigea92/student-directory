d85fdb753b3b794707f805fce6d0b077d89abae2
Craig Eaton <craigea92@gmail.com>
2021-05-17 12:28:29 +0100

    Updated directory-copy.rb - Added each.with_index to print a number before each student name

--- directory-copy.rb
+++ directory-copy.rb
@@ -25,8 +25,8 @@ def print_header
   end
 # and then print the student names
   def print(students)
-    students.each do |student|
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    students.each.with_index(1) do |student, index|
+      puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
 # finally print the student count
