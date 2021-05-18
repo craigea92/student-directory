55046c83134ed80863819a18ee2e9b70310b32e6
Craig Eaton <craigea92@gmail.com>
2021-05-18 20:03:49 +0100

    Updated directory.rb - Refactored Code

--- directory.rb
+++ directory.rb
@@ -1,67 +1,65 @@
-def interactive_menu
-  # create an empty array
-  students = []
-  loop do
-  # 1. print the menu and ask the user what to do
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit" # 9 because we'll be adding more items
-  # 2. read the input and save it into a variable
-  selection = gets.chomp
-  # 3. do what the user has asked
-  case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't what you meant, try again"
-    end
-  # 4. repeat from step 1
-  end
-end
+@students = [] # an empty array accessible to all methods
 
-# lets get the names of students from user input
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-# create an empty array
-  students = []
-# get the first name
+  # get the first name
   name = gets.chomp
-# while the name is not empty, repeat this code
+  # while the name is not empty, repeat this code
   while !name.empty? do
-# add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-# get another name from the user
+    # add the student hash to the array
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
     name = gets.chomp
   end
-# return the array of students
-  students
 end
 
-# start by printing the header
-def print_header
-    puts "The students of Villains Academy"
-    puts "-------------"
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
   end
-# and then print the student names
-  def print(students)
-    students.each do |student|
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end
-  end
-# finally print the student count
-  def print_footer(names)
-    puts "Overall, we have #{names.count} great students"
-  end
+end
 
-# nothing happens until we call the methods
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
 
 interactive_menu
-students = input_students
