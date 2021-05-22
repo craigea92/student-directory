b8fed33e6bd090fa1a9b5d58b619b9df10017ed4
Craig Eaton <craigea92@gmail.com>
2021-05-22 10:42:50 +0100

    Updated directory.rb - Refactored Code, Reduced the use of "students"

--- directory.rb
+++ directory.rb
@@ -2,12 +2,12 @@
 
 def interactive_menu
   loop do
-    print_menu
+    menu
     process(STDIN.gets.chomp)
   end
 end
 
-def print_menu
+def menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
@@ -17,65 +17,55 @@ end
 
 def process(selection)
   case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when "3"
-    save_students
-  when "4"
-    load_students
-  when "9"
-    exit # this will cause the program to terminate
+  when "1" then input
+  when "2" then show
+  when "3" then save
+  when "4" then load
+  when "9" then exit # this will cause the program to terminate
   else
     puts "I don't know what you meant, try again"
   end
 end
 
-def input_students
+def input
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # get the first name
-  name = STDIN.gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    add_students(name)
+  name = STDIN.gets.chomp # get the first name
+  while !name.empty? do # while name is not empty, repeat this code
+    add(name) # add the student hash to the array
     puts "Now we have #{@students.count} students"
-    # get another name from the user
-    name = STDIN.gets.chomp
+    name = STDIN.gets.chomp # get another name from the user
   end
 end
 
-def add_students(name, cohort = "november")
+def add(name, cohort = "november")
   @students << {name: name, cohort: cohort.to_sym}
 end
 
-def show_students
-  print_header
-  print_student_list
-  print_footer
+def show
+  header
+  student_list
+  footer
 end
 
-def print_header
+def header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print_student_list
+def student_list
   @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    puts ""
   end
 end
 
-def print_footer
+def footer
   puts "Overall, we have #{@students.count} great students"
+  puts ""
 end
 
-def save_students
-  # open the file for writing
-  file = File.open("students.csv", "w")
+def save
+  file = File.open("students.csv", "w") # open the file for writing
   # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
@@ -85,20 +75,20 @@ def save_students
   file.close
 end
 
-def load_students(filename = "students.csv")
+def load(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(',')
-    add_students(name, cohort)
+    add(name, cohort)
   end
   file.close
 end
 
-def try_load_students
+def try_load
   filename = ARGV.first# first argument from the command line
   filename = "students.csv" if filename.nil? # get out of the method if it isn't given
   if File.exists?(filename) # if it exists
-    load_students(filename)
+    load(filename)
      puts "Loaded #{@students.count} from #{filename}"
   else # if it doesn't exist
     puts "Sorry, #{filename} doesn't exist."
@@ -106,5 +96,5 @@ def try_load_students
   end
 end
 
-try_load_students
+try_load
 interactive_menu
